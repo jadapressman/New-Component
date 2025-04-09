a2f636cc21dc7f0b1094e8a8b7b07978dd575208
jadapressman <pressman.29@buckeyemail.osu.edu>
2025-04-09 09:59:16 -0400

    added the keyword this

--- src/PlaylistSecondary.java
+++ src/PlaylistSecondary.java
@@ -10,25 +10,25 @@ public abstract class PlaylistSecondary<String> implements Playlist<String> {
 
     @Override
     public String entry(int pos) {
-        String song = Playlist.remove(pos);
+        String song = this.Playlist.remove(pos);
         Playlist.add(pos, song);
         return song;
     }
 
     @Override
     public void swapSong(int pos1, int pos2) {
-        String song2 = Playlist.remove(pos2);
-        String song1 = Playlist.remove(pos1);
+        String song2 = this.Playlist.remove(pos2);
+        String song1 = this.Playlist.remove(pos1);
 
-        Playlist.add(song2, pos1);
-        Playlist.add(song1, pos2);
+        this.Playlist.add(song2, pos1);
+        this.Playlist.add(song1, pos2);
     }
 
 @Override
      public void append(Playlist<String> s) {
-        int i = Playlist.length();
+        int i = this.Playlist.length();
         for(String x : s){
-            Playlist.add(s,i)
+            this.Playlist.add(s,i)
             i++;
         }
         s.clear();
@@ -38,7 +38,7 @@ public abstract class PlaylistSecondary<String> implements Playlist<String> {
      public void insert(int pos, Playlist<String> s) {
         int i = pos;
         for(String x : s){
-            Playlist.add(s,i)
+            this.Playlist.add(s,i)
             i++;
         }
         s.clear();
@@ -48,8 +48,8 @@ public abstract class PlaylistSecondary<String> implements Playlist<String> {
      public void extract(int pos1, int pos2, Playlist<String> s){
         int j = 0;
         for(int i = pos1; i < pos2; i++){
-            String song = Playlist.remove(i);
-            s.add(song, j)
+            String song = this.Playlist.remove(i);
+            s.Playlist.add(song, j)
             j++;
         }
      }
@@ -57,7 +57,7 @@ public abstract class PlaylistSecondary<String> implements Playlist<String> {
 @Override
     public static boolean lookUp(String){
         boolean isIn = false;
-        for (String x : Playlist) {
+        for (String x : this.Playlist) {
             if (x.equals(song)) {
                 isIn = true;
             }
@@ -68,8 +68,8 @@ public abstract class PlaylistSecondary<String> implements Playlist<String> {
     @Override
     public String toString() {
         StringBuffer buf = new StringBuffer();
-        for (int i = 0; i < Playlist.length(); i++) {
-            String song = Playlist.remove(0);
+        for (int i = 0; i < this.Playlist.length(); i++) {
+            String song = this.Playlist.remove(0);
             buf.append(song);
             buf.append(", ");
         }
